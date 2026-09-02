ae5961eac000eba27aea152193e7b054cba4f730
Abbas <mirzaabbas7095@gmail.com>
2026-09-02 23:24:35 +0530

    Time: 6 ms (73.34%) | Memory: 61.2 MB (44.47%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let l = 0, r = s.length - 1;
+    let count = 0;
+
+    while (l < r) {
+        if (s[l] == s[r]) {
+            l++; r--;
+        } else {
+            return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+        }
+    }
+
+    return true;
+};
+
+function isPalindrome(s, l, r) {
+    while (l < r) {
+        if (s[l++] != s[r--]) {
+            return false;
+        }
+    }
+
+    return true;
+}
