8e19d8f0600cc3b28bef2404e634b4737b0a2dcc
Abbas <mirzaabbas7095@gmail.com>
2026-09-09 21:30:50 +0530

    Time: 2593 ms (5.00%) | Memory: 91.3 MB (9.31%) - LeetSync

---
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.java
@@ -0,0 +1,45 @@
+class Solution {
+    static long mod = (long) 1e9 + 7;
+
+    public long myPow(long x, long n) {
+        if (n == 0 || x == 1) return 1;
+        if (n == 1) return x;
+
+        return pow(x % mod, n) % mod;
+    }
+
+    private long pow(long x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if ((n & 1) == 0) {
+            return pow(((x % mod) * (x % mod)) % mod, n / 2);
+        }
+
+        return (x * pow(((x % mod) * (x % mod)) % mod, n / 2)) % mod;
+    }
+
+    public int sumDecoded(long[] nums) {
+        long sum = 0;
+
+        for (long num : nums) {
+            long w = num % 10;
+            long d = (long) Math.floor(num / 10);
+            String n = String.valueOf(d);
+
+            if (w > n.length()) continue;
+
+            long x = Long.parseLong(n.substring(0, (int) w));
+            long y = Long.parseLong(n.substring((int) w));
+
+            System.out.println(num);
+            System.out.println(x);
+            System.out.println(y);
+
+            sum = (sum + myPow(x, y)) % mod;
+        }
+
+        return (int) sum;
+    }
+}
