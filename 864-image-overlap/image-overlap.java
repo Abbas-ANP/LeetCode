b78b917dabadd1126a8eb745ab0f48dea9a08ecf
Abbas <mirzaabbas7095@gmail.com>
2026-09-13 14:20:56 +0530

    Time: 209 ms (14.01%) | Memory: 47.5 MB (25.38%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,29 +1,22 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        List<int[]> i1 = new ArrayList<>();
-        List<int[]> i2 = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (img1[i][j] == 1) {
-                    i1.add(new int[] {i, j});
-                }
-
-                if (img2[i][j] == 1) {
-                    i2.add(new int[] {i, j});
-                }
-            }
-        }
-
         int count = 0;
         Map<String, Integer> map = new HashMap<>();
 
-        for (int p1[] : i1) {
-            for (int p2[] : i2) {
-                String diff = (p1[0] - p2[0]) + ", " +  (p1[1] - p2[1]);
-                map.put(diff, map.getOrDefault(diff, 0) + 1);
-                count = Math.max(count, map.get(diff));
+        for (int i1 = 0; i1 < n; i1++) {
+            for (int j1 = 0; j1 < n; j1++) {
+                if (img1[i1][j1] == 1) {
+                    for (int i2 = 0; i2 < n; i2++) {
+                        for (int j2 = 0; j2 < n; j2++) {
+                            if (img2[i2][j2] == 1) {
+                                String point = (i1 - i2) + ", " + (j1 - j2);
+                                map.put(point, map.getOrDefault(point, 0) + 1);
+                                count = Math.max(count, map.get(point));
+                            }
+                        }
+                    }
+                }
             }
         }
 
