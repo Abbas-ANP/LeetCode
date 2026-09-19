8cc5ddf8e26ed737bbc237be4f30a4cfe8482585
Abbas <mirzaabbas7095@gmail.com>
2026-09-19 20:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (38.66%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int x = 0, y = 0;
+
+        if (x1 > xCenter) x = x1;
+        else if (x2 < xCenter) x = x2;
+        else x = xCenter;
+
+        if (y1 > yCenter) y = y1;
+        else if (y2 < yCenter) y = y2;
+        else y = yCenter;
+
+        int d = (int)Math.sqrt(Math.pow(x - xCenter, 2) + Math.pow(y - yCenter, 2));
+        return d <= radius;
+    }
+}
