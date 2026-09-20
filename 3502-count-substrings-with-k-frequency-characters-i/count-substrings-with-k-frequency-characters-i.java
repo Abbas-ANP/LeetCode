9fdc45552a8a063b7a9562efd4fabe824f22acbe
Abbas <mirzaabbas7095@gmail.com>
2026-09-20 18:34:44 +0530

    Time: 4 ms (51.44%) | Memory: 44.4 MB (30.58%) - LeetSync

--- 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.java
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.java
@@ -1,16 +1,21 @@
 class Solution {
     public int numberOfSubstrings(String s, int k) {
         int n = s.length();
+        if (k == 1) return n * (n + 1) / 2;
         int count = 0;
+        int l = 0;
+        Map<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            Map<Character, Integer> map = new HashMap<>();
-            for (int j = i; j < n; j++) {
-                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-                if (map.get(s.charAt(j)) >= k) {
-                    count += (n - j);
-                    break;
-                }
+        for (int r = 0; r < n; r++) {
+            char ch = s.charAt(r);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while (map.get(ch) >= k) {
+                count += (n - r);
+
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                if (map.get(s.charAt(l)) == 0) map.remove(s.charAt(l));
+                l++;
             }
         }
 
